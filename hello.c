cf0cfe98945db873ebf22310841e3699ebe6233a
Naomi Post <npost3@huskers.unl.edu>
2023-08-24 09:46:43 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Naomi Post
+ *         npost3@huskers.unl.edu
+ * Date: 2023/08/24
+ *
+ * A program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Naomi Post\n");
+
+  return 0;
+}
